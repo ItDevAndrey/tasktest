f9a4a5915f649297fd3b7b6073f7d883dff1a02c
ItDevAndrey <itdevshopify@gmail.com>
2024-07-10 17:52:16 +0300

    Added section

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -2,17 +2,39 @@ import Swiper from 'swiper';
 import SwiperCore, { Navigation, Pagination } from 'swiper/core';
 
 SwiperCore.use([Navigation, Pagination]);
-var swiper = new Swiper(".mySwiper", {
-  cssMode: true,
-  slidesPerView: 4,
-  spaceBetween: 10,
+
+const infoSliderSwiper = new Swiper('.info-slider-swiper', {
+  slidesPerView: 1,
+  speed: 600,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
-});
+  }
+})
+
+const infoCategories = document.querySelectorAll('.info-slider-titles h2');
+
+if (infoCategories) {
+  infoCategories[0].classList.add('is-active');
+  const activeItem = {
+    elem: infoCategories[0]
+  }
+
+  infoCategories.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      activeItem.elem.classList.remove('is-active');
+      item.classList.add('is-active');
+      activeItem.elem = item;
+
+      infoSliderSwiper.slideTo(index);
+    })
+  });
+
+  infoSliderSwiper.on('slideChange', () => {
+    activeItem.elem.classList.remove('is-active');
+
+    infoCategories[infoSliderSwiper.realIndex].classList.add('is-active');
+    activeItem.elem = infoCategories[infoSliderSwiper.realIndex];
+    console.log(infoCategories[infoSliderSwiper.realIndex]);
+  })
+}
